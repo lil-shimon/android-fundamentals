93d132acfea463ff5974d31b099b492bfcba885d
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-04-28 16:30:23 +0900

    feat: 🎸 2 dice

--- src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -9,12 +9,14 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage: ImageView
+    lateinit var diceImageTop: ImageView
+    lateinit var diceImageButtom: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImageTop = findViewById(R.id.dice_image_top)
+        diceImageButtom = findViewById(R.id.dice_image_bottom)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
@@ -23,15 +25,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val diceResource = when ((1..6).random()) {
+        diceImageTop.setImageResource(getRandomDiceImage())
+        diceImageButtom.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
+        return when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
+
         }
-        diceImage.setImageResource(diceResource)
     }
 
 //    private fun countUp() {
